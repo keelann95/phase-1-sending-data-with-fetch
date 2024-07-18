f7ff77e0ddba665ce7f9412afad9f7b0a9e3c208
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-07-18 16:31:30 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    // Define the endpoint URL
+    const url = 'http://localhost:3000/users';
+    
+    // Create the fetch request
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Manipulate DOM to append new id
+      document.body.innerHTML += `<p>${data.id}</p>`;
+    })
+    .catch(error => {
+      // Handle errors by appending error message to DOM
+      document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+  }
+  
